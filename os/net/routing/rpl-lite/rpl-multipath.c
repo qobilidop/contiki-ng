bb35b7fc9c91954b62d03c5dd0b68f27f66d2a5a
ShuyiNi <nishuyi0105@gmail.com>
2020-03-12 05:23:21 -0700

    fix segfault issue, add random choose route scheme, not test. Problem: tx_exp not update, always 0

--- os/net/routing/rpl-lite/rpl-multipath.c
+++ os/net/routing/rpl-lite/rpl-multipath.c
@@ -11,13 +11,19 @@
 #include "net/routing/rpl-lite/rpl.h"
 #include "net/ipv6/uip-ds6-route.h"
 #include "net/nbr-table.h"
+#include "lib/random.h"
 
 /* Log configuration */
 #include "sys/log.h"
-#define LOG_MODULE "RPL"
+#define LOG_MODULE "M-RPL"
 #define LOG_LEVEL LOG_LEVEL_RPL
 
-#define CONGESTION_INTERVAL_S ((1UL << RPL_MULTIPATH_CONGESTION_INTERVAL) * CLOCK_SECOND)
+#define MRPL_RANDOM MRPL_CONF_RANDOM
+#define MRPL_RANDOM_TIMES 3
+
+#define CONGESTION_INTERVAL_S ((1UL << RPL_MULTIPATH_CONGESTION_INTERVAL) * CLOCK_SECOND / 1000)
+
+#if RPL_WITH_MULTIPATH
 
 typedef struct {
   uint16_t rx;
@@ -45,6 +51,7 @@ void send_congestion_notification_immediately(void);
 void rpl_multipath_start(void);
 void rpl_multipath_stop(void);
 rpl_nbr_t *find_alt_route(void);
+rpl_nbr_t *find_alt_route_random(int times);
 
 /*---------------------------------------------------------------------------*/
 /*------------------------------- Main ------------------------------------- */
@@ -52,37 +59,40 @@ rpl_nbr_t *find_alt_route(void);
 void
 rpl_multipath_init(void)
 {
+  LOG_INFO("use multipath rpl.\n");
   rpl_multipath_reset();
   rpl_multipath_stop();
   netstack_sniffer_add(&rpl_multipath_sniffer);
   ctimer_set(&congestion_timer, CONGESTION_INTERVAL_S, handle_congestion_timer, NULL);
+  LOG_INFO("after use multipath rpl.\n");
 }
 /*---------------------------------------------------------------------------*/
 static void
 rpl_multipath_reset(void)
 {
-  rpl_nbr_t *nbr;
-
   packet_counter.rx = 0;
   packet_counter.rx_exp = 0;
   packet_counter.tx_dao = 0;
 
-  curr_instance.congested = false;
+  //do not need to reset congestion status
+  //rpl_nbr_t *nbr;
+  // curr_instance.congested = false;
 
-  nbr = nbr_table_head(rpl_neighbors);
-  while(nbr != NULL) {
-    nbr->cn = false;
-  }
+  // nbr = nbr_table_head(rpl_neighbors);
+  // while(nbr != NULL) {
+  //   nbr->cn = false;
+  // }
 }
 /*---------------------------------------------------------------------------*/
 static void
 handle_congestion_timer(void *ptr)
 {
+  LOG_INFO("handle congestion timer\n");
   detect_congestion();
   if (curr_instance.congested) {
     send_congestion_notification();
   }
-  if (!curr_instance.dag.preferred_parent->cn) {
+  if (curr_instance.dag.preferred_parent != NULL && !curr_instance.dag.preferred_parent->cn) {
     rpl_multipath_stop();
   }
   rpl_multipath_reset();
@@ -94,10 +104,15 @@ handle_congestion_timer(void *ptr)
 void
 detect_congestion(void)
 {
+  LOG_INFO("detect_congestion\n");
   uint16_t threshold = packet_counter.rx_exp * RPL_MULTIPATH_CONGESTION_THRESHOLD / RPL_MULTIPATH_CONGESTION_RANGE;
+  LOG_INFO("rx: %u, threshold: %u\n", packet_counter.rx, packet_counter.rx_exp);
   if(packet_counter.rx < threshold) {
     curr_instance.congested = true;
     return;
+  }
+  else{
+    curr_instance.congested = false;
   }
 
   uint8_t total, congested;
@@ -171,7 +186,13 @@ void
 rpl_multipath_start(void)
 {
   rpl_multipath_stop();
-  rpl_nbr_t *alt_route = find_alt_route();
+  rpl_nbr_t *alt_route;
+  #if !MRPL_RANDOM
+    alt_route= find_alt_route();
+  #else 
+    alt_route= find_alt_route_random(MRPL_RANDOM_TIMES);
+  #endif
+  LOG_INFO_6ADDR(rpl_neighbor_get_ipaddr(alt_route));
   if (alt_route != NULL) {
     uip_ds6_defrt_add(rpl_neighbor_get_ipaddr(alt_route), 0);
     rpl_multipath.alt_route = alt_route;
@@ -202,6 +223,30 @@ find_alt_route(void)
   }
   return NULL;
 }
+
+rpl_nbr_t *
+find_alt_route_random(int times)
+{
+  int i;
+  rpl_nbr_t *nbr;
+  for(i=0;i<times;i++){
+    int rand = random_rand() % rpl_neighbor_count();
+    nbr = nbr_table_head(rpl_neighbors);
+    while(nbr != NULL) {
+      if(rand == 0){
+        if (rpl_neighbor_is_parent(nbr) && !nbr->cn &&
+            nbr != curr_instance.dag.preferred_parent) {
+          return nbr;
+        }
+        break;
+      }
+      nbr = nbr_table_next(rpl_neighbors, nbr);
+      --rand;
+    }
+  }
+  return NULL;
+}
+#endif /* RPL_WITH_MULTIPATH */
 /*---------------------------------------------------------------------------*/
 
 /** @} */
