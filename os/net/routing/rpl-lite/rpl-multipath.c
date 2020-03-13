42ad6d75c9431a6f5fa8bb7d0e0ce11e52edff1c
qobilidop <qobilidop@gmail.com>
2020-03-13 16:42:08 -0700

    Tune the multipath strategy

--- os/net/routing/rpl-lite/rpl-multipath.c
+++ os/net/routing/rpl-lite/rpl-multipath.c
@@ -18,7 +18,7 @@
 #define LOG_MODULE "M-RPL"
 #define LOG_LEVEL LOG_LEVEL_RPL
 
-#define MRPL_RANDOM MRPL_CONF_RANDOM
+#define MRPL_RANDOM 0
 #define MRPL_RANDOM_TIMES 3
 
 #define CONGESTION_INTERVAL_S ((1UL << RPL_MULTIPATH_CONGESTION_INTERVAL) * CLOCK_SECOND / 1000)
@@ -80,8 +80,8 @@ handle_congestion_timer(void *ptr)
   LOG_INFO("handle congestion timer\n");
   LOG_INFO("rx: %u, rx_exp: %u, tx_dao: %u\n",
     packet_counter.rx, packet_counter.rx_exp, packet_counter.tx_dao);
-  LOG_INFO("congested: %u, on: %u, skip: %u\n",
-    curr_instance.congested, rpl_multipath.on, rpl_multipath.skip);
+  LOG_INFO("congested: %u, on: %u, skip: %u, alt_route set: %u\n",
+    curr_instance.congested, rpl_multipath.on, rpl_multipath.skip, rpl_multipath.alt_route != NULL);
   detect_congestion();
   if (curr_instance.congested) {
     send_congestion_notification();
@@ -100,16 +100,20 @@ void
 detect_congestion(void)
 {
   LOG_INFO("detect congestion\n");
-  uint16_t threshold = packet_counter.rx_exp * RPL_MULTIPATH_CONGESTION_THRESHOLD / RPL_MULTIPATH_CONGESTION_RANGE;
+
+  /* Assume not congested in the beginning */
+  curr_instance.congested = false;
+
+  /* Detect if self is congested */
+  uint16_t thresh_min = packet_counter.rx_exp * RPL_MULTIPATH_CONGESTION_THRESH_MIN / RPL_MULTIPATH_CONGESTION_THRESH_SCALE;
+  uint16_t thresh_max = packet_counter.rx_exp * RPL_MULTIPATH_CONGESTION_THRESH_MAX / RPL_MULTIPATH_CONGESTION_THRESH_SCALE;
   LOG_INFO("rx: %u, threshold: %u\n", packet_counter.rx, packet_counter.rx_exp);
-  if(packet_counter.rx < threshold) {
+  if(packet_counter.rx < thresh_min || packet_counter.rx > thresh_max) {
     curr_instance.congested = true;
     return;
   }
-  else{
-    curr_instance.congested = false;
-  }
 
+  /* Detect if most neighbors are congested */
   uint8_t total, congested;
   rpl_nbr_t *nbr;
   total = 0;
@@ -123,6 +127,7 @@ detect_congestion(void)
   }
   if(total < (congested << 1)) {
     curr_instance.congested = true;
+    return;
   }
 }
 /*---------------------------------------------------------------------------*/
@@ -189,6 +194,8 @@ void
 rpl_multipath_start(void)
 {
   rpl_nbr_t *alt_route;
+  LOG_INFO("multipath start\n");
+  LOG_INFO("neighbor count: %u\n", rpl_neighbor_count());
   #if !MRPL_RANDOM
     alt_route= find_alt_route();
   #else
@@ -196,6 +203,7 @@ rpl_multipath_start(void)
   #endif
   LOG_INFO_6ADDR(rpl_neighbor_get_ipaddr(alt_route));
   if (alt_route != NULL) {
+    LOG_INFO("alt route found\n");
     uip_ds6_defrt_add(rpl_neighbor_get_ipaddr(alt_route), 0);
     rpl_multipath.alt_route = alt_route;
     rpl_multipath.on = true;
@@ -205,6 +213,7 @@ rpl_multipath_start(void)
 void
 rpl_multipath_stop(void)
 {
+  LOG_INFO("multipath stop\n");
   rpl_multipath.on = false;
   rpl_multipath.skip = false;
   if(rpl_multipath.alt_route != NULL) {
@@ -218,14 +227,17 @@ rpl_nbr_t *
 find_alt_route(void)
 {
   rpl_nbr_t *nbr = nbr_table_head(rpl_neighbors);
+  rpl_nbr_t *nbr_backup = NULL;
   while(nbr != NULL) {
-    if (rpl_neighbor_is_parent(nbr) && !nbr->cn &&
-        nbr != curr_instance.dag.preferred_parent) {
-      return nbr;
+    if (rpl_neighbor_is_parent(nbr) && nbr != curr_instance.dag.preferred_parent) {
+      nbr_backup = nbr;
+      if (!nbr->cn) {
+        return nbr;
+      }
     }
     nbr = nbr_table_next(rpl_neighbors, nbr);
   }
-  return NULL;
+  return nbr_backup;
 }
 
 rpl_nbr_t *
